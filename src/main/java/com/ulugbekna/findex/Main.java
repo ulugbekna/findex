8b4c8501bf2ebcc95c36adfc8b9233fb878d62bb
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-11-16 16:40:44 +0100

    - don't fail if `nJobs <= 0` - make sure to query on startup only if previously indexed on startup - use a Result instead of a boolean as the return type of `index`

--- src/main/java/com/ulugbekna/findex/Main.java
+++ src/main/java/com/ulugbekna/findex/Main.java
@@ -48,10 +48,13 @@ public class Main implements Runnable {
         final var indexer = new Indexer<>(byWordLexer);
 
         ExecutorService pool;
-        if (nJobs <= 0)
-            throw new IllegalArgumentException("The number of jobs must be positive"); // TODO: improve
-        else
-            pool = Executors.newFixedThreadPool(nJobs);
+        if (nJobs <= 0) {
+            int nCores = Runtime.getRuntime().availableProcessors();
+            System.out.println("Number of jobs needs to be positive. " +
+                    "Defaulting to the number of processor cores available: " + nCores);
+            nJobs = nCores;
+        }
+        pool = Executors.newFixedThreadPool(nJobs);
 
         // index files provided as a CLI argument
         if (filesToIndex.size() > 0) {
@@ -77,33 +80,41 @@ public class Main implements Runnable {
             System.out.println("  *** Indexing complete ***");
         }
 
-        for (var keyword : keywordsToQuery) {
-            System.out.println("Querying \"" + keyword + "\":");
-            query(indexer, keyword);
+        // respond to queries if indexed files previously
+        if (keywordsToQuery.size() > 0) {
+            if (filesToIndex.size() > 0) {
+                for (var keyword : keywordsToQuery) {
+                    System.out.println("Querying \"" + keyword + "\":");
+                    query(indexer, keyword);
+                }
+            } else {
+                System.out.println("Querying without indexing before that will not yield any results.");
+            }
         }
 
         if (runAsRepl) runAsRepl(indexer, pool);
     }
 
-    private boolean index(Indexer<String, ?> indexer, Path file) {
+    private Result index(Indexer<String, ?> indexer, Path file) {
         try {
             System.out.println("  Indexing file at path: " + file);
             indexer.index(file);
-            return true;
+            return Result.Success;
         } catch (FileNotFoundException e) {
-            System.out.println("File for indexing not found at path " + file);
+            System.out.println("  File for indexing not found at path " + file);
         } catch (IOException e) {
-            System.out.print("There was an internal error on reading file for indexing at path " + file);
+            System.out.print("  There was an internal error on reading file for indexing at path " + file);
         }
-        return false;
+        return Result.Failure;
     }
 
     private void query(Indexer<String, ?> indexer, String keyword) {
         var r = indexer.query(keyword);
         if (r.isEmpty()) {
-            System.out.println("Couldn't find any files related to keyword " + keyword);
+            System.out.println("  Couldn't find any files related to keyword " + keyword);
         } else {
-            for (var tfa : r) System.out.println("Found in file: " + tfa.path());
+            for (var tfa : r)
+                System.out.println("  Found in file: " + tfa.path());
         }
     }
 
@@ -129,12 +140,14 @@ public class Main implements Runnable {
                     case "query", "q" -> query(indexer, inputParam);
                     case "index", "i" -> {
                         final var file = Paths.get(inputParam);
-                        var wasSuccessfullyIndexed = pool.submit(() -> index(indexer, file));
+                        var indexingResult = pool.submit(() -> index(indexer, file));
                         try {
-                            if (wasSuccessfullyIndexed.get())
+                            if (indexingResult.get() == Result.Success) {
                                 System.out.println("  *** Indexing complete ***");
+                            }
                         } catch (InterruptedException | ExecutionException e) {
-                            System.out.print("There was an internal error on reading file for indexing at path " + file);
+                            System.out.print("  There was an internal error on reading file for indexing " +
+                                    "at path " + file);
                         }
                     }
                     default -> incorrectReplInput();
@@ -143,6 +156,8 @@ public class Main implements Runnable {
             System.out.print("> ");
         }
     }
+
+    enum Result {Success, Failure}
 }
 
 /*
